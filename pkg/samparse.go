360c35dc3fdd0964e5813f65c0ea11288e7c930c
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-11-13 18:03:18 -0700

    commented out all of samparse

--- pkg/samparse.go
+++ pkg/samparse.go
@@ -1,37 +1,37 @@
 package fastats
 
-func ParseSamHeading(s string) string {
-	return s[1:]
-}
-
-type SamAlignment struct {
-	Qname string
-	Flag uint16
-	Rname string
-	Pos int64
-	Mapq int64
-	CIGAR string
-	Rnext string
-	Pnext int64
-	Tlen int64
-	Seq string
-	Qual string
-}
-
-func ParseSamAlignment(s string) 
-
-func ParseSamEntry
-
-// Col	Field	Type	Brief description
-// 1	QNAME	String	Query template NAME
-// 2	FLAG	Int	bitwise FLAG
-// 3	RNAME	String	References sequence NAME
-// 4	POS	Int	1- based leftmost mapping POSition
-// 5	MAPQ	Int	MAPping Quality
-// 6	CIGAR	String	CIGAR string
-// 7	RNEXT	String	Ref. name of the mate/next read
-// 8	PNEXT	Int	Position of the mate/next read
-// 9	TLEN	Int	observed Template LENgth
-// 10	SEQ	String	segment SEQuence
-// 11	QUAL	String	ASCII of Phred-scaled base QUALity+33
+// func ParseSamHeading(s string) string {
+// 	return s[1:]
+// }
 // 
+// type SamAlignment struct {
+// 	Qname string
+// 	Flag uint16
+// 	Rname string
+// 	Pos int64
+// 	Mapq int64
+// 	CIGAR string
+// 	Rnext string
+// 	Pnext int64
+// 	Tlen int64
+// 	Seq string
+// 	Qual string
+// }
+// 
+// func ParseSamAlignment(s string) 
+// 
+// func ParseSamEntry
+// 
+// // Col	Field	Type	Brief description
+// // 1	QNAME	String	Query template NAME
+// // 2	FLAG	Int	bitwise FLAG
+// // 3	RNAME	String	References sequence NAME
+// // 4	POS	Int	1- based leftmost mapping POSition
+// // 5	MAPQ	Int	MAPping Quality
+// // 6	CIGAR	String	CIGAR string
+// // 7	RNEXT	String	Ref. name of the mate/next read
+// // 8	PNEXT	Int	Position of the mate/next read
+// // 9	TLEN	Int	observed Template LENgth
+// // 10	SEQ	String	segment SEQuence
+// // 11	QUAL	String	ASCII of Phred-scaled base QUALity+33
+// // 

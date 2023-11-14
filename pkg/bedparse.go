b00ce0e6887fe888952b1c1953d017e510660c86
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-11-13 18:03:08 -0700

    slightly updated bedparse to use scanone

--- pkg/bedparse.go
+++ pkg/bedparse.go
@@ -21,15 +21,43 @@ type BedEntry[FieldsT any] struct {
 	Fields FieldsT
 }
 
-func Scan(line []string, ptrs ...any) (n int, err error) {
+func ScanOne(field string, ptr any) (n int, err error) {
+	if s, ok := ptr.(*string); ok {
+		*s = field
+		return 1, nil
+	}
+	nread, e := fmt.Sscanf(field, "%v", ptr)
+	return nread, e
+}
+
+func ScanOneDot(field string, ptr any) (n int, err error) {
+	n, e := ScanOne(field, ptr)
+	if e == nil {
+		return n, nil
+	}
+	if field == "." {
+		return 1, nil
+	}
+	return n, fmt.Errorf("ScanOneDot: field: %v; ptr: %v; err: %w", field, ptr, e)
+}
+
+func ScanF(f func(string, any) (int, error), line []string, ptrs ...any) (n int, err error) {
 	for i, ptr := range ptrs {
-		nread, e := fmt.Sscanf(line[i], "%v", ptr)
+		nread, e := f(line[i], ptr)
 		n += nread
 		if e != nil {
 			return n, e
 		}
 	}
 	return n, nil
+}
+
+func Scan(line []string, ptrs ...any) (n int, err error) {
+	return ScanF(ScanOne, line, ptrs...)
+}
+
+func ScanDot(line []string, ptrs ...any) (n int, err error) {
+	return ScanF(ScanOneDot, line, ptrs...)
 }
 
 func ParseBedEntry[FT any](line []string, fieldParse func([]string) (FT, error)) (BedEntry[FT], error) {

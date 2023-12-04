b5e55d211a966f22b08d7b18b338a0cb917ffc31
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-12-04 16:01:06 -0700

    updated fawin to be safe to use

--- pkg/fawin.go
+++ pkg/fawin.go
@@ -8,7 +8,11 @@ func Wins(start, end, size, step int64) *iter.Iterator[Span] {
 	return &iter.Iterator[Span]{Iteratef: func(yield func(Span) error) error {
 		var i int64
 		for i = start; i < end; i += step {
-			e := yield(Span{i, i + step})
+			winend := i + size
+			if winend > end {
+				winend = end
+			}
+			e := yield(Span{i, winend})
 			if e != nil {
 				return e
 			}
@@ -17,17 +21,26 @@ func Wins(start, end, size, step int64) *iter.Iterator[Span] {
 	}}
 }
 
-type FaView struct {
-	FaEntry
-	Span
-}
-
-func FaEntryWins(fe FaEntry, size int64, step int64) *iter.Iterator[FaView] {
-	return &iter.Iterator[FaView]{Iteratef: func(yield func(FaView) error) error {
+func FaEntryWins(fe FaEntry, size int64, step int64) *iter.Iterator[BedEntry[string]] {
+	return &iter.Iterator[BedEntry[string]]{Iteratef: func(yield func(BedEntry[string]) error) error {
 		wins := Wins(0, int64(len(fe.Seq)), size, step)
 		return wins.Iterate(func(s Span) error {
-			fv := FaView {fe, s}
+			fv := BedEntry[string]{
+				ChrSpan: ChrSpan{fe.Header, Span{s.Start, s.End}},
+				Fields: fe.Seq[s.Start : s.End],
+			}
 			return yield(fv)
+		})
+	}}
+}
+
+func FaWins(fa iter.Iter[FaEntry], size int64, step int64) *iter.Iterator[BedEntry[string]] {
+	return &iter.Iterator[BedEntry[string]]{Iteratef: func(yield func(BedEntry[string]) error) error {
+		return fa.Iterate(func(fe FaEntry) error {
+			wins := FaEntryWins(fe, size, step)
+			return wins.Iterate(func(view BedEntry[string]) error {
+				return yield(view)
+			})
 		})
 	}}
 }

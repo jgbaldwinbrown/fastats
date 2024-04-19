49aebf836a8227b2fdba7329a80834868c2860e6
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-04-19 17:53:26 -0600

    fixed fawrite

--- pkg/fawrite.go
+++ pkg/fawrite.go
@@ -3,11 +3,12 @@ package fastats
 import (
 	"io"
 	"github.com/jgbaldwinbrown/iter"
+	"fmt"
 )
 
 func WriteFaEntries(w io.Writer, fs ...FaEntry) error {
 	for _, f := range fs {
-		if _, e := fmt.Printf(">%s\n%s\n", fs.Header, fs.Seq); e != nil {
+		if _, e := fmt.Printf(">%s\n%s\n", f.Header, f.Seq); e != nil {
 			return e
 		}
 	}
@@ -17,5 +18,5 @@ func WriteFaEntries(w io.Writer, fs ...FaEntry) error {
 func WriteFa(w io.Writer, it iter.Iter[FaEntry]) error {
 	return it.Iterate(func(f FaEntry) error {
 		return WriteFaEntries(w, f)
-	}
+	})
 }

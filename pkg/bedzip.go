32966922f9e6d1ed12d9054bd3c96cdb5e130e17
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-11-26 16:37:09 -0700

    bedzip in progress

--- pkg/bedzip.go
+++ pkg/bedzip.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"log"
 	"bufio"
+	"flag"
+	"io"
 
 	"github.com/jgbaldwinbrown/iterh"
 )
 
 func BedZip[B1 BedEnter[[]string], B2 BedEnter[[]string]](it1 iter.Seq[B1], it2 iter.Seq[B2]) []BedEntry[[]string] {
-	matched := ZipMatches(it1, it2)
+	matched := iterh.ZipMatches(it1, it2)
 	out := make([]BedEntry[[]string], 0, len(matched))
 	for chrspan, tup := range matched {
 		if tup.Ok {
@@ -23,15 +25,63 @@ func BedZip[B1 BedEnter[[]string], B2 BedEnter[[]string]](it1 iter.Seq[B1], it2 
 	return out
 }
 
-func FullBedZip() {
-	args := os.Args
-	if len(args) < 3 {
-		log.Printf("usage: %s bed1.bed bed2.bed\n", args[0])
-		log.Fatal(fmt.Errorf("missing arguments"))
+type bedZipFlags struct {
+	Header1 bool
+	Header2 bool
+	Bed1 string
+	Bed2 string
+}
+
+func ChompLine(r io.ByteReader) error {
+	for {
+		val, e := r.ReadByte()
+		if e == io.EOF {
+			break
+		}
+		if e != nil {
+			return e
+		}
+		if val == '\n' {
+			break
+		}
 	}
+	return nil
+}
+
+func ParseBedFlatHeader(r io.Reader) iter.Seq2[BedEntry[[]string], error] {
+	return func(y func(BedEntry[[]string], error) bool) {
+		_, e := ChompLine(r)
+		if e != nil && !y(BedEntry[[]string]{}, e) {
+			return
+		}
+		for b, err := range ParseBedFlat(r) {
+			if !y(b, err) {
+				return
+			}
+		}
+	}
+}
+
+func FullBedZip() {
+	var f bedZipFlags
+	flag.BoolVar(&f.Header1, "h1", false, "first bed file has a header")
+	flag.BoolVar(&f.Header2, "h2", false, "second bed file has a header")
+	flag.StringVar(&f.Bed1, "h1", "", "first bed file")
+	flag.StringVar(&f.Bed2, "h2", "", "second bed file")
+	flag.Parse()
+
 	var err error
-	b1 := iterh.BreakOnError(iterh.PathIter(args[1], ParseBedFlat), &err)
-	b2 := iterh.BreakOnError(iterh.PathIter(args[2], ParseBedFlat), &err)
+	var b1, b2 iter.Seq2[BedEntry[[]string], error]
+	if (f.Header1) {
+		b1 := iterh.BreakOnError(iterh.PathIter(f.Bed1, ParseBedFlatHeader), &err)
+	} else {
+		b1 := iterh.BreakOnError(iterh.PathIter(f.Bed1, ParseBedFlat), &err)
+	}
+	if (f.Header2) {
+		b2 := iterh.BreakOnError(iterh.PathIter(f.Bed2, ParseBedFlatHeader), &err)
+	} else {
+		b2 := iterh.BreakOnError(iterh.PathIter(f.Bed2, ParseBedFlat), &err)
+	}
 
 	out := BedZip(b1, b2)
 	if err != nil {

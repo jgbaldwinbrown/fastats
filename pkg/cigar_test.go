a6cdb635c0ca6bf38bc8567f7e2a8389017adf46
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2026-02-06 16:28:17 -0700

    working cigar simplifier

--- pkg/cigar_test.go
+++ pkg/cigar_test.go
@@ -22,3 +22,19 @@ func TestParseCIGAR(t *testing.T) {
 		t.Errorf("got %v != exp %v", got, exp)
 	}
 }
+
+func TestSimplifyCIGAR(t *testing.T) {
+	in := "33M22M45D3I2S"
+	exp := []CIGAREntry{
+		CIGAREntry{'M', 55},
+		CIGAREntry{'D', 50},
+	}
+	got, e := ParseCIGAR(in)
+	if e != nil {
+		t.Error(e)
+	}
+	simple := SimplifyCIGAR(got, 10, CIGARLeft)
+	if !reflect.DeepEqual(simple, exp) {
+		t.Errorf("simple %v != exp %v", simple, exp)
+	}
+}
